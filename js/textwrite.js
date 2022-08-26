1380c3e149ba3672589f08a03c196bf0ce28c27e
kyungtae <108638458+KyungTaeP@users.noreply.github.com>
2022-08-26 16:54:09 +0900

    클릭이벤트 및 디자인수정 필요
    클릭이벤트 및 디자인수정 필요

--- js/textwrite.js
+++ js/textwrite.js
@@ -1,6 +1,7 @@
 const content = "프론트엔드 개발자를 꿈꾸는 \n 박경태입니다.";
 
 const text = document.querySelector(".text");
+
 let i = 0;
 
 function typing(){
@@ -8,7 +9,7 @@ function typing(){
     let txt = content.charAt(i);
     text.innerHTML += txt=== "\n" ? "<br/>": txt;
     i++;
-    }
+    }    
 }
 
 setInterval(typing, 200)
